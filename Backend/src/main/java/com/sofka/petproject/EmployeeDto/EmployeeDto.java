4e2bc7403bfd6650dbed33f5a6423babf9a39534
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-30 14:10:55 -0500

    Creación de clase DTO

--- Backend/src/main/java/com/sofka/petproject/EmployeeDto/EmployeeDto.java
+++ Backend/src/main/java/com/sofka/petproject/EmployeeDto/EmployeeDto.java
@@ -1,4 +1,16 @@
 package com.sofka.petproject.EmployeeDto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDto {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+
 }
