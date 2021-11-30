e78a7f05db6b4d8debf25308f493138e8e19ba67
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-30 12:42:54 -0500

    Se crea la clase entidad con sus respectivos atributos

--- Backend/src/main/java/com/sofka/petproject/EmployeeModel/EmployeeModel.java
+++ Backend/src/main/java/com/sofka/petproject/EmployeeModel/EmployeeModel.java
@@ -1,4 +1,25 @@
 package com.sofka.petproject.EmployeeModel;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
 public class EmployeeModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
 }
