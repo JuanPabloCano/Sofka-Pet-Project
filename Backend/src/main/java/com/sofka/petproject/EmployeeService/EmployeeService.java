48078e0840d7b38b792803af32bb48f2bd23d8e0
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-30 14:13:02 -0500

    Creación de clase Service con sus respectivos métodos

--- Backend/src/main/java/com/sofka/petproject/EmployeeService/EmployeeService.java
+++ Backend/src/main/java/com/sofka/petproject/EmployeeService/EmployeeService.java
@@ -1,4 +1,39 @@
 package com.sofka.petproject.EmployeeService;
 
-public class EmployeeService {
+import com.sofka.petproject.EmployeeModel.EmployeeModel;
+import com.sofka.petproject.EmployeeRepository.EmployeeAppInterface;
+import com.sofka.petproject.EmployeeRepository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class EmployeeService implements EmployeeAppInterface {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    @Override
+    public List<EmployeeModel> list() {
+        return (List<EmployeeModel>) employeeRepository.findAll();
+    }
+
+    @Override
+    public EmployeeModel save(EmployeeModel employeeModel) {
+        return employeeRepository.save(employeeModel);
+    }
+
+    @Override
+    public void delete(Long id) {
+        employeeRepository.delete(getById(id));
+    }
+
+    @Override
+    public EmployeeModel getById(Long id) {
+        return employeeRepository.findById(id).orElseThrow();
+    }
 }
