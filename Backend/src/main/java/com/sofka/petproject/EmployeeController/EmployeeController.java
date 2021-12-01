db13c240fcf11f12112d84801d47d047130967ec
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-12-01 13:47:27 -0500

    Se corrige el código con mejores prácticas

--- Backend/src/main/java/com/sofka/petproject/EmployeeController/EmployeeController.java
+++ Backend/src/main/java/com/sofka/petproject/EmployeeController/EmployeeController.java
@@ -3,15 +3,12 @@ package com.sofka.petproject.EmployeeController;
 import com.sofka.petproject.EmployeeDto.EmployeeDto;
 import com.sofka.petproject.EmployeeModel.EmployeeModel;
 import com.sofka.petproject.EmployeeService.EmployeeService;
-import org.modelmapper.ModelMapper;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+@AllArgsConstructor
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
@@ -19,50 +16,32 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @Autowired
-    private ModelMapper modelMapper;
-
     @GetMapping(value = "api/v1/employees")
-    public List<EmployeeDto> getAllEmployees() {
-        return employeeService.list()
-                .stream()
-                .map(mapper -> modelMapper.map(mapper, EmployeeDto.class))
-                .collect(Collectors.toList());
+    public Iterable <EmployeeDto> getAllEmployees(){
+        return employeeService.getAllEmployees();
     }
 
+
     @PostMapping(value = "api/v1/employee")
-    public ResponseEntity<EmployeeDto> save(@RequestBody EmployeeDto employeeDto) {
-
-        // Mapper DTO a Entity
-        EmployeeModel employeeModel = modelMapper.map(employeeDto, EmployeeModel.class);
-        EmployeeModel employeeSave = employeeService.save(employeeModel);
-
-        // Mapper Entity a Dto
-        EmployeeDto employeeDtoMap = modelMapper.map(employeeSave, EmployeeDto.class);
-
-        return new ResponseEntity<>(employeeDtoMap, HttpStatus.CREATED);
+    public ResponseEntity<EmployeeModel> save(@RequestBody EmployeeDto employeeDto){
+        return employeeService.save(employeeDto);
     }
 
     @PutMapping(value = "api/v1/employee")
-    public EmployeeModel update(@RequestBody EmployeeModel employeeModel) {
-        if (employeeModel.getId() != null) {
-            return employeeService.save(employeeModel);
+    public ResponseEntity<EmployeeModel> update(@RequestBody EmployeeDto employeeDto){
+        if (employeeDto.getId() != null){
+            return employeeService.save(employeeDto);
         }
-        throw new RuntimeException("Id no encontrado");
+        throw new RuntimeException("Id no encontrado para actualizar");
     }
 
     @DeleteMapping(value = "api/{id}/employee")
-    public void delete(@PathVariable("id") Long id) {
-        employeeService.delete(id);
+    public void deleteById(@PathVariable(value = "id")Long id){
+        employeeService.deleteById(id);
     }
 
     @GetMapping(value = "api/{id}/employee")
-    public ResponseEntity<EmployeeDto> getByID(@PathVariable("id") Long id) {
-        EmployeeModel employeeModel = employeeService.getById(id);
-
-        // Mapper Entity a Dto
-        EmployeeDto employeeDto = modelMapper.map(employeeModel, EmployeeDto.class);
-
-        return ResponseEntity.ok().body(employeeDto);
+    public ResponseEntity<EmployeeDto> getById(@PathVariable(value = "id")Long id){
+        return employeeService.getById(id);
     }
 }
