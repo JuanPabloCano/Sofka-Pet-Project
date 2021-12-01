3bcd0e6a583eb55c0c88e37579944c0bb4046b2f
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-12-01 13:43:11 -0500

    Se crea la interfaz para el uso de MapStruct y se añade la dependencia

---
+++ Backend/src/main/java/com/sofka/petproject/EmployeeMapper/EmployeeDtoMapper.java
@@ -0,0 +1,24 @@
+package com.sofka.petproject.EmployeeMapper;
+
+import com.sofka.petproject.EmployeeDto.EmployeeDto;
+import com.sofka.petproject.EmployeeModel.EmployeeModel;
+import com.sofka.petproject.EmployeeValidator.EmployeeValidationException;
+import com.sofka.petproject.EmployeeValidator.EmployeeValidator;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(uses = {EmployeeValidator.class}, componentModel = "spring")
+public interface EmployeeDtoMapper {
+
+
+    // Conversor entidad a DTO
+    EmployeeDto entityToDto(EmployeeModel employeeModel) throws EmployeeValidationException;
+
+    //Conversor DTO a entidad
+    EmployeeModel dtoToEntity(EmployeeDto employeeDto) throws EmployeeValidationException;
+
+    // Conversor de entidad a DTO para mostrar la lista completa
+    List<EmployeeDto> entitiesToDto(List<EmployeeModel> employeeModels) throws EmployeeValidationException;
+
+}
