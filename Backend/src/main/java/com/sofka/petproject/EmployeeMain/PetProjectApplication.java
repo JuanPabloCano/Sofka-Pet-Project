f45f1b3291fde90a58d249886767e345285e3064
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-12-01 13:44:29 -0500

    Se elimina el método modelMapper

--- Backend/src/main/java/com/sofka/petproject/EmployeeMain/PetProjectApplication.java
+++ Backend/src/main/java/com/sofka/petproject/EmployeeMain/PetProjectApplication.java
@@ -1,17 +1,10 @@
 package com.sofka.petproject.EmployeeMain;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PetProjectApplication {
-
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetProjectApplication.class, args);
