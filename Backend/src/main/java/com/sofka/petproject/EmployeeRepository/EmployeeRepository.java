35158fba5b19e7aabfb38d161f7e2bc754704614
GustavoOrozco <gustavo.orozcom68@gmail.com>
2021-12-01 23:32:29 -0500

    Delete Backend directory

--- Backend/src/main/java/com/sofka/petproject/EmployeeRepository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sofka.petproject.EmployeeRepository;
-
-import com.sofka.petproject.EmployeeModel.EmployeeModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<EmployeeModel, Long> {
-}
