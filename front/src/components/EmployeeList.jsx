0689bd28b0fde7788aa848a3525ab400d97fd506
Tavo826 <gustavo.orozcom68@gmail.com>
2021-12-02 13:11:19 -0500

    bara de busqueda basica

--- front/src/components/EmployeeList.jsx
+++ front/src/components/EmployeeList.jsx
@@ -6,6 +6,10 @@ import EmployeeService from "../services/EmployeeService";
 function EmployeeList() {
 
     const [employees, setEmployees] = useState([]);
+    const [employeesFilter, setEmployeesFilter] = useState([]);
+
+    const [busqueda, setBusqueda] = useState("");
+
     let navigate = useNavigate();
 
     useEffect(() => {
@@ -17,6 +21,7 @@ function EmployeeList() {
             if (response.ok) {
                 response.json().then(data => {
                     setEmployees(data);
+                    setEmployeesFilter(data);
                 })
             }
         })
@@ -40,6 +45,20 @@ function EmployeeList() {
         navigate(`/employee/view/${id}`, { replace: true });
     }
 
+    const handleInputChange = (event) => {
+        setBusqueda(event.target.value);
+        filter(event.target.value);
+    }
+
+    const filter = (term) => {
+        let searchResult = employeesFilter.filter((elemento) => {
+            if(elemento.firstName.toLowerCase().includes(term.toLowerCase()) || elemento.lastName.toLowerCase().includes(term.toLowerCase())) {
+                return elemento;
+            }
+        })
+        setEmployees(searchResult);
+    }
+
 
     return (
         <div className="mt-5">
@@ -49,39 +68,50 @@ function EmployeeList() {
                 <button className="btn btn-primary btn-inicio" onClick={() => { addEmployee() }}> Agregar empleado </button>
             </div>
 
-            <table className="table table-bordered">
-                <thead>
-                    <tr className="text-center">
-                        <th scope="col">Id</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Apellido</th>
-                        <th scope="col">Correo</th>
-                        <th scope="col">Opciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        employees.map((employee) => {
-                            return (
-                                <tr className="text-center" key={employee.id}>
-                                    <td>{employee.id}</td>
-                                    <td>{employee.firstName}</td>
-                                    <td>{employee.lastName}</td>
-                                    <td>{employee.emailId}</td>
-                                    <td>
-                                        <div className="text-center">
-                                            <button className="btn btn-light btn-edit" onClick={() => editEmployee(employee.id)}>Editar</button>
-                                            <button style={{ marginLeft: "10px" }} className="btn btn-dark btn-del" onClick={() => deleteEmployee(employee.id)}>Eliminar</button>
-                                            <button style={{ marginLeft: "10px" }} className="btn btn-dark btn-ver" onClick={() => viewEmployee(employee.id)}>Ver</button>
-                                        </div>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
+            {/* Barra de búsqueda */}
 
-                </tbody>
-            </table>
+            <div>
+                <input type="text" className="form-control" 
+                value={busqueda} placeholder="Buscar" 
+                onChange={handleInputChange}/>
+            </div>
+
+            <div className="table-responsive">
+                <table className="table table-bordered">
+                    <thead>
+                        <tr className="text-center">
+                            <th scope="col">Id</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Correo</th>
+                            <th scope="col">Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            employees.map((employee) => {
+                                return (
+                                    <tr className="text-center" key={employee.id}>
+                                        <td>{employee.id}</td>
+                                        <td>{employee.firstName}</td>
+                                        <td>{employee.lastName}</td>
+                                        <td>{employee.emailId}</td>
+                                        <td>
+                                            <div className="text-center">
+                                                <button className="btn btn-light btn-edit" onClick={() => editEmployee(employee.id)}>Editar</button>
+                                                <button style={{ marginLeft: "10px" }} className="btn btn-dark btn-del" onClick={() => deleteEmployee(employee.id)}>Eliminar</button>
+                                                <button style={{ marginLeft: "10px" }} className="btn btn-dark btn-ver" onClick={() => viewEmployee(employee.id)}>Ver</button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     )
 }
---
+++ front/src/components/EmployeeSearch.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EmployeeSearch = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default EmployeeSearch;
